0847603b8e6fc7a7f72743c3786ff08931557efb
davidbirenzi <d.birenzi@alustudent.com>
2024-10-20 18:44:44 +0300

    form page

---
+++ server.js
@@ -0,0 +1,13 @@
+const e = require("express");
+
+function SendMail() {
+    var params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value
+    }
+    emailjs.send("service_u90ghzc", "template_4z39uyq", params)
+    .then(function (res) {
+        alert("success! " + res.status);
+    })
+}
